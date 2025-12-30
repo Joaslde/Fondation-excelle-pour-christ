16874e3b99686856c41dfa15b6747525ca842321
TonNomGitHub <tonemail@gmail.com>
2025-12-30 21:31:22 +0100

    Fix: Ajout de postcss-preset-env pour supporter la syntaxe de couleur CSS Color Level 5 (hsl(from...))

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,15 @@
 // postcss.config.cjs
 module.exports = {
   plugins: {
+    // 1. Assurez-vous que postcss-preset-env est le premier
+    // Il gère les fonctionnalités CSS futures (comme la syntaxe 'from hsl')
+    'postcss-preset-env': {
+      stage: 3, // Supporte les spécifications W3C stables
+      features: {
+        'css-has': false // Optionnel, si vous avez des conflits avec :has()
+      }
+    },
+    // 2. Les plugins classiques de Tailwind et Autoprefixer suivent
     tailwindcss: {},
     autoprefixer: {},
   },
